9116a0008200b70a81ae2b996b6870592d2624aa
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-04-13 10:41:04 +0530

    add statup path

---
+++ startup/routes.js
@@ -0,0 +1,16 @@
+const admin = require('../routes/Admin')
+const login = require('../routes/auth')
+const project = require('../routes/Projects')
+const skill = require('../routes/Skills')
+const experience = require('../routes/Experience')
+const express = require('express')
+
+module.exports = function (app){
+    app.use(express.json())
+    app.use('/api/project',project)
+    app.use('/api/skill',skill)
+    app.use('/api/experience',experience)
+    app.use('/api/login',login)
+    app.use('/api/admin',admin)
+
+}
