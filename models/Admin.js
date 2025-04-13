23f45695590a8565582910ab853d8fabb23cad82
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-04-13 10:33:30 +0530

    complete schema and model of admin

---
+++ models/Admin.js
@@ -0,0 +1,40 @@
+const jwt = require('jsonwebtoken')
+const Joi = require('joi')
+const mongoose  = require('mongoose')
+
+const AdminSchema = new mongoose.Schema({
+    email: {
+        type : String,
+        unique: true,
+        required: true,
+        minlength: 5,
+        maxlength: 100
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength:1024
+    },
+    isAdmin: {
+        type :Boolean,
+        default: true
+    }
+})
+AdminSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign({_id: this._id, isAdmin: this.isAdmin}, process.env.portfolio_jwtPrivateKey)
+    return token
+}
+
+const AdminModel = mongoose.model("Admin",AdminSchema)
+
+function validateAdmin(admin){
+    const Schema  = Joi.object({
+        email: Joi.string().min(5).max(255).required(),
+        password:Joi.string().min(5).max(255).required()
+    })
+    return Schema.validate(admin)
+}
+
+module.exports.Admin = AdminModel
+module.exports.validateAdmin = validateAdmin
