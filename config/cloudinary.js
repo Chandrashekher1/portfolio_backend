048d3e5e0ca3d6454a05226c70f65d7ab76ae090
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-04-13 10:43:23 +0530

    ...

---
+++ config/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: 'dt9a9xhz1',
+    api_key: '271614543354583',
+    api_secret: 'b-shBrRBRdjQ4iohbD3Sma1rOiA'
+});
+
+module.exports = cloudinary;
---
+++ config/storage.js
@@ -0,0 +1,14 @@
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('./cloudinary'); 
+const multer = require('multer')
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'Projects', 
+        allowed_formats: ['jpg', 'png', 'jpeg'],
+    }
+});
+
+const upload = multer({ storage });
+module.exports = upload;
