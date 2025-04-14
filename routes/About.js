e7a3a8405b4ef0e24568de25ff093894469ab92f
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-04-14 14:24:46 +0530

    add about routes

---
+++ routes/About.js
@@ -0,0 +1,56 @@
+const {About,validate} =  require('../models/About')
+const express = require('express')
+const router = express.Router()
+
+router.get('/', async(req,res) => {
+    const about = await About.find()
+    if(!about) return res.status(400).json({message: "Something wrong."})
+    res.send(about)
+})
+
+router.post('/', async(req,res) => {
+    try{
+        let about = new About({
+            name: req.body.name,
+            profession: req.body.profession,
+            bio: req.body.bio,
+            socialLinks: {
+                github: req.body.socialLinks.github,
+                linkedin: req.body.socialLinks.linkedin,
+                twitter: req.body.socialLinks.twitter,
+                portfolio: req.body.socialLinks.portfolio
+            }
+        })
+        console.log(about);
+        
+        about = await about.save()
+        res.send(about)
+    }
+    catch(error){
+        res.status(500).json({message: error.details[0].message})
+    }
+})
+
+router.put('/:id', async(req,res) => {
+    try{
+        let updated = await About.findByIdAndUpdate(req.params.id,req.body, {new: true})
+        if(!updated) return res.status(400).json({message:"given data not found."})
+        res.send(updated)
+    }
+    catch(error){
+        res.status(500).json({message: "Something went wrong while updating."})
+    }
+})
+
+router.delete('/:id', async(req,res) => {
+    try{
+        const about = await About.findByIdAndDelete(req.params.id)
+        if(!about) return res.status(404).json({message: "Not found."})
+        res.send(about)
+    }
+    catch(error){
+        res.status(500).json({message: error.details[0].message})
+    }
+})
+
+module.exports = router
