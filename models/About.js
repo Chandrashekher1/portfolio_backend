d299ef708c08a76a68d25115cc0e0b1ab358b891
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-04-14 14:24:06 +0530

    add about model

---
+++ models/About.js
@@ -0,0 +1,43 @@
+const Joi = require('joi')
+const mongoose = require('mongoose')
+
+const AboutSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    profession: {
+        type: [String],
+        required:true,
+    },
+    bio :{
+        type: String,
+        required: true
+    },
+    socialLinks: {
+        github: {type: String, required:true},
+        linkedin: {type: String, required:true},
+        twitter: {type: String, required:true},
+        portfolio: {type: String, required:true}
+    }
+}, {timestamps:true})
+
+const aboutModel = mongoose.model("About",AboutSchema)
+
+function AboutValidation(about){
+    const Schema = Joi.object({
+        name: Joi.string().required(),
+        profession: Joi.array().items(Joi.string()).required(),
+        bio: Joi.string().required(),
+        socialLinks: Joi.object({
+            github: Joi.string().uri().required(),
+            linkedin: Joi.string().uri().required(),
+            twitter: Joi.string().uri().required(),
+            portfolio: Joi.string().uri().required()
+        }).required()
+    })
+    return Schema.validate(about)
+}
+
+module.exports.About = aboutModel
+module.exports.validate = AboutValidation
