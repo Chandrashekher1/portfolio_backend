56a7c6ed7eb1a4f3d7bd3a7367cce3cce8264924
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-04-19 09:17:18 +0530

    integrate image upload feature

--- models/About.js
+++ models/About.js
@@ -18,6 +18,9 @@ const AboutSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    image: {
+        type: String
+    },
     socialLinks: {
         github: {type: String, required:true},
         linkedin: {type: String, required:true},
